711c64a5a866cde49134277f528a7652596bc3da
Dillon Brock <mdillon1297@gmail.com>
2022-08-03 21:26:12 -0700

    wrote test for city list route

---
+++ __tests__/us_cities.test.js
@@ -0,0 +1,23 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('city routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('GET should return a list of cities', async () => {
+    const res = await request(app).get('/cities');
+    expect(res.status).toBe(200);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      city: expect.any(String),
+      state: expect.any(String),
+      population: expect.any(Number)
+    });
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
