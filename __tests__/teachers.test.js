f3e9f1ba56a8ff955cd0d89c57abd14ec2a45cbe
Dillon Brock <mdillon1297@gmail.com>
2022-08-03 17:38:32 -0700

    wrote test for teacher list route

---
+++ __tests__/teachers.test.js
@@ -0,0 +1,24 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('teacher routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('GET should return a list of teachers', async () => {
+    const res = await request(app).get('/teachers');
+    expect(res.status).toBe(200);
+    expect(res.body.find((teacher) => teacher.first_name === 'Helen')).toEqual({
+      id: '1',
+      first_name: 'Helen',
+      last_name: 'Spencer-Wallace',
+      subject: 'Analog Modular Synthesis',
+      school: 'Portland Community College'
+    });
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
